c2e8463a50da622e7cc4f0a4983e0994c466365d
NickRuffini <njruffini@gmail.com>
2020-05-04 09:57:34 -0500

    Lab 37 Finished

--- Ksu.Cis300.SortLibrary.Tests/SortTests.cs
+++ Ksu.Cis300.SortLibrary.Tests/SortTests.cs
@@ -14,15 +14,77 @@ namespace Ksu.Cis300.SortLibrary.Tests
     public class SortTests
     {
         /// <summary>
-        /// Tests SelectionSort on a 10-element array.
+        /// Tests sorting a 1-element array.
         /// </summary>
         [Test, Timeout(1000)]
-        public void TestASelectionSortSmall()
+        public void TestASortOne()
+        {
+            int[] data = new int[] { 1 };
+            int[] result = new int[] { 1 };
+            Sort.MergeSort(result);
+            Assert.That(result, Is.EquivalentTo(data));
+        }
+
+        /// <summary>
+        /// Tests sorting a 2-element unordered array.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestBSortTwoUnordered()
+        {
+            int[] data = new int[] { 7, 2 };
+            int[] result = new int[2];
+            data.CopyTo(result, 0);
+            Sort.MergeSort(result);
+            Assert.That(result, Is.Ordered.And.EquivalentTo(data));
+        }
+
+        /// <summary>
+        /// Tests sorting a 2-element ordered array.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestBSortTwoOrdered()
+        {
+            int[] data = new int[] { 2, 7 };
+            int[] result = (int[])data.Clone();
+            Sort.MergeSort(result);
+            Assert.That(result, Is.Ordered.And.EquivalentTo(data));
+        }
+
+        /// <summary>
+        /// Tests sorting a 4-element array in which neither half contains elements less
+        /// than all the elements in the other half.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestCSortFourMixed()
+        {
+            int[] data = new int[] { 7, 2, 5, 3 };
+            int[] result = (int[])data.Clone();
+            Sort.MergeSort(result);
+            Assert.That(result, Is.Ordered.And.EquivalentTo(data));
+        }
+
+        /// <summary>
+        /// Tests sorting a 4-element array in reverse order.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestCSortFourReversed()
+        {
+            int[] data = new int[] { 7, 5, 3, 2 };
+            int[] result = (int[])data.Clone();
+            Sort.MergeSort(result);
+            Assert.That(result, Is.Ordered.And.EquivalentTo(data));
+        }
+
+        /// <summary>
+        /// Tests sorting a 10-element array.
+        /// </summary>
+        [Test, Timeout(1000)]
+        public void TestDSortTen()
         {
             int[] data = new int[] { 3, -7, 0, 11, -7, 8, 4, 8, -2, 6 };
             int[] result = new int[data.Length];
             data.CopyTo(result, 0);
-            Sort.SelectionSort(result);
+            Sort.MergeSort(result);
             Assert.That(result, Is.Ordered.And.EquivalentTo(data));
         }
 
@@ -44,67 +106,41 @@ namespace Ksu.Cis300.SortLibrary.Tests
         }
 
         /// <summary>
-        /// Tests SelectionSort on a random list of 100 elements.
+        /// Tests sorting a random list of 100 elements.
         /// </summary>
         [Test, Timeout(1000)]
-        public void TestBSelectionSort100()
+        public void TestESort100()
         {
             List<int> data = new List<int>();
             List<int> result = new List<int>();
             Initialize(data, result, 100);
-            Sort.SelectionSort(result);
+            Sort.MergeSort(result);
             Assert.That(result, Is.Ordered.And.EquivalentTo(data));
         }
 
-        [Test, Timeout(20000)]
+        [Test, Timeout(1000)]
         /// <summary>
-        /// Tests SelectionSort on a random list of 30000 elements.
+        /// Tests sorting a random list of 30000 elements.
         /// </summary>
-        public void TestCSelectionSort30000()
+        public void TestFSort30000()
         {
             List<int> data = new List<int>();
             List<int> result = new List<int>();
             Initialize(data, result, 30000);
-            Sort.SelectionSort(result);
+            Sort.MergeSort(result);
             Assert.That(result, Is.Ordered);
         }
 
         /// <summary>
-        /// Tests InsertionSort on a 10-element array.
+        /// Tests sorting a random list of one million elements.
         /// </summary>
-        [Test, Timeout(1000)]
-        public void TestDInsertionSortSmall()
-        {
-            int[] data = new int[] { 3, -7, 0, 11, -7, 8, 4, 8, -2, 6 };
-            int[] result = new int[data.Length];
-            data.CopyTo(result, 0);
-            Sort.InsertionSort(result);
-            Assert.That(result, Is.Ordered.And.EquivalentTo(data));
-        }
-
-        /// <summary>
-        /// Tests InsertionSort on a random list of 100 elements.
-        /// </summary>
-        [Test, Timeout(1000)]
-        public void TestEInsertionSort100()
+        [Test, Timeout(5000)]
+        public void TestGSortMillion()
         {
             List<int> data = new List<int>();
             List<int> result = new List<int>();
-            Initialize(data, result, 100);
-            Sort.InsertionSort(result);
-            Assert.That(result, Is.Ordered.And.EquivalentTo(data));
-        }
-
-        [Test, Timeout(20000)]
-        /// <summary>
-        /// Tests InsertionSort on a random list of 30000 elements.
-        /// </summary>
-        public void TestFInsertionSort30000()
-        {
-            List<int> data = new List<int>();
-            List<int> result = new List<int>();
-            Initialize(data, result, 30000);
-            Sort.InsertionSort(result);
+            Initialize(data, result, 1000000);
+            Sort.MergeSort(result);
             Assert.That(result, Is.Ordered);
         }
     }
--- Ksu.Cis300.SortLibrary/Sort.cs
+++ Ksu.Cis300.SortLibrary/Sort.cs
@@ -1,5 +1,5 @@
 ﻿/* Sort.cs
- * Author: Rod Howell
+ * Author: Nick Ruffini
  */
 using System;
 using System.Collections.Generic;
@@ -65,6 +65,94 @@ namespace Ksu.Cis300.SortLibrary
                 }
                 list[j] = temp;
             }
+        }
+
+        /// <summary>
+        /// Merges two adjacent sorted portions of list into a single sorted portion!
+        /// </summary>
+        /// <param name="list"> Overall list that we are working with (entire thing) </param>
+        /// <param name="start"> Starting index of the section to merge! </param>
+        /// <param name="len1"> Size of the first section we are merging </param>
+        /// <param name="len2"> Size of the second section we are merging </param>
+        private static void Merge(IList<int> list, int start, int len1, int len2)
+        {
+            int[] temp = new int[len1 + len2];
+            int pos1 = start;
+            int pos2 = start + len1;
+            int tempPos = 0;
+
+            while ((pos1 < start + len1) && (pos2 < start + len1 + len2))
+            {
+                if (list[pos1] < list[pos2])
+                {
+                    temp[tempPos] = list[pos1];
+                    pos1++;
+                    tempPos++;
+                }
+                else
+                {
+                    temp[tempPos] = list[pos2];
+                    pos2++;
+                    tempPos++;
+                }
+            }
+            while (pos1 < start + len1)
+            {
+                temp[tempPos] = list[pos1];
+                pos1++;
+                tempPos++;
+            }
+            while (pos2 < start + len1 + len2)
+            {
+                temp[tempPos] = list[pos2];
+                pos2++;
+                tempPos++;
+            }
+
+            int position = start;
+            for (int i = 0; i < temp.Length; i++)
+            {
+                list[position] = temp[i];
+                position++;
+            }
+        }
+
+        /// <summary>
+        /// Sorts a portion of a list using Merge Sort!
+        /// </summary>
+        /// <param name="list"> List that we are applying Merge Sort to </param>
+        /// <param name="start"> The first position of the section we are sorting </param>
+        /// <param name="len"> Number of elements starting from start that we are soriting </param>
+        private static void MergeSort(IList<int> list, int start, int len)
+        {
+            if (len >= 2)
+            {
+                /*List<int> firstHalf = new List<int>();
+                for (int i = start; i < len / 2; i++)
+                {
+                    firstHalf.Add(list[i]);
+                }
+
+                List<int> secondHalf = new List<int>();
+                for (int j = start + len / 2; j < len; j++)
+                {
+                    secondHalf.Add(list[j]);
+                }*/
+
+                MergeSort(list, start, len / 2);
+                MergeSort(list, start + len / 2, len - len / 2);
+
+                Merge(list, start, len/2, len - len / 2);
+            }
+        }
+
+        /// <summary>
+        /// Calls MergeSort on the entire list
+        /// </summary>
+        /// <param name="list"> List that we are merge sorting! </param>
+        public static void MergeSort(IList<int> list)
+        {
+            MergeSort(list, 0, list.Count);
         }
     }
 }
